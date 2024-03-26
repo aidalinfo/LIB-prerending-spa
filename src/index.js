5717134d58dabdf752ca974accbf8bca9251f4b4
Killian - Aidalinfo <146849979+Killian-Aidalinfo@users.noreply.github.com>
2024-03-26 16:04:01 +0100

    Update index.js
    Remove headless and remove execute path

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ class SPAPrerenderer {
       console.log(`HTTP Server running on http://localhost:${port}`);
 
       const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/google-chrome',
+        headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
       });
       const page = await browser.newPage();
